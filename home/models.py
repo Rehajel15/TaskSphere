42dda5e57763ee47e40d0147fd0166b5062049c7
Rehajel <143653636+Rehajel15@users.noreply.github.com>
2024-07-04 02:53:56 +0200

    Migrated the changes

--- home/models.py
+++ home/models.py
@@ -1,3 +1,4 @@
+# models.py
 from django.db import models
 import uuid
 from datetime import datetime
@@ -7,9 +8,10 @@ class Table_taskColumn(models.Model):
     column_name = models.CharField(max_length=30)
     column_description = models.CharField(max_length=150)
     column_color = models.CharField(max_length=10)
+    table = models.ForeignKey('Table', related_name='taskColumns', on_delete=models.CASCADE) 
 
     def __str__(self):
-        return str(self.column_name) + ' || ' + str(self.id)
+        return f"{self.column_name} || {self.id}"
     
 class Table_task(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
@@ -19,26 +21,26 @@ class Table_task(models.Model):
     current_worker = models.CharField(max_length=30)
     deadLine = models.DateTimeField()
     created_on = models.DateTimeField(default=datetime.now, editable=False)
+    table = models.ForeignKey('Table', related_name='tasks', on_delete=models.CASCADE) 
 
     def __str__(self):
-        return str(self.task_name) + ' || ' + str(self.id)
+        return f"{self.task_name} || {self.id}"
 
 class Table(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    task_column = models.ForeignKey(Table_taskColumn, related_name='Table_taskColumn', on_delete=models.CASCADE)
-    task = models.ForeignKey(Table_task, related_name='Table_task', on_delete=models.CASCADE)
+    task_column = models.ForeignKey(Table_taskColumn, related_name='tables', on_delete=models.CASCADE)
+    task = models.ForeignKey(Table_task, related_name='tables', on_delete=models.CASCADE)
+    group = models.ForeignKey('Group', related_name='tables', on_delete=models.CASCADE)  
     
     def __str__(self):
         return str(self.id)
 
-# Create your models here.
 class Group(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     givenID = models.CharField(max_length=40)
     group_name = models.CharField(max_length=30)
     group_description = models.CharField(max_length=150)
     created_on = models.DateTimeField(default=datetime.now, editable=False)
-    data = models.ForeignKey(Table, related_name='data_tables', on_delete=models.CASCADE)
 
     def __str__(self):
-        return str(self.column_name) + ' || ' + str(self.givenID)
+        return f"{self.group_name} || {self.givenID}"
