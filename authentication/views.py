c412013eb589473ac8310150b754b7cce64a368f
Rehajel <143653636+Rehajel15@users.noreply.github.com>
2024-08-05 22:35:59 +0200

    Finished the first version of the security check of the sign up page.

--- authentication/views.py
+++ authentication/views.py
@@ -15,10 +15,10 @@ def SignIn(request):
         return redirect('/authentication/')
     else:
         if request.method == 'POST':
-            email = request.POST.get('emailInput')
-            password = request.POST.get('passwordInput')
-            
-            user = authenticate(username=email, password=password)
+            email = request.POST['emailInput']
+            password = request.POST['passwordInput']
+
+            user = authenticate(request, email=email, password=password)
 
             if user is None:
                 messages.error(request, "Wrong email or password. Please try again.")
@@ -34,53 +34,83 @@ def SignIn(request):
 
 def SignUp(request):
     if request.user.is_authenticated:
-        messages.error(request, f"You can't sign in because you already are. To do this please sign out.")
+        messages.error(request, "You can't sign up because you already are. To do this please sign out.")
         return redirect('/authentication/')
-    else:
-        if request.method == 'POST':
-            post = {
-                'emailInput': request.POST.get('emailInput'),
-                'passwordInput': request.POST.get('passwordInput'),
-                'passwordInput2': request.POST.get('passwordInput2'),
-                'firstnameInput': request.POST.get('firstnameInput'),
-                'lastnameInput': request.POST.get('lastnameInput'),
-                'biographyInput': request.POST.get('biographyInput'),
-                'profilePictureInput': request.FILES.get('profilePictureInput')
-            }
+    
+    if request.method == 'POST':
+        post = {
+            'emailInput': request.POST.get('emailInput'),
+            'passwordInput': request.POST.get('passwordInput'),
+            'passwordInput2': request.POST.get('passwordInput2'),
+            'firstnameInput': request.POST.get('firstnameInput'),
+            'lastnameInput': request.POST.get('lastnameInput'),
+            'biographyInput': request.POST.get('biographyInput'),
+            'profilePictureInput': request.FILES.get('profilePictureInput')
+        }
 
-            if not post['passwordInput'] == post['passwordInput2']:
-                messages.error(request, "You repeated the wrong password. Please try again.")
-                post_data = {key: value for key, value in post.items() if key != 'passwordInput2'}
-                return render(request, 'signup.html', {'profile': post_data})
-            
-            elif CustomUser.objects.filter(email=post['emailInput']):
-                messages.error(request, "Account with this email adress already exists.")
-                post_data = {key: value for key, value in post.items() if key != 'emailInput'}
-                return render(request, 'signup.html', {'profile': post_data})
-            
-            elif len(post['emailInput']) >= 30:
-                messages.error(request, "Email adress is too long.")
-                post_data = {key: value for key, value in post.items() if key != 'emailInput'}
-                return render(request, 'signup.html', {'profile': post_data})
-            
-            elif len(post['passwordInput']) < 10 or not any(char.isalpha() for char in post['passwordInput']) or not any(char.isdigit() for char in post['passwordInput']):
-                messages.error(request, "Password is too short or includes no numbers and letters")
-                post_data = {key: value for key, value in post.items() if key not in ['passwordInput', 'passwordInput2']}
-                return render(request, 'signup.html', {'profile': post_data})
-            
-            else:
-                new_user = CustomUser.objects.create(
-                    email = post['emailInput'],
-                    password = post['passwordInput'],
-                    firstname = post['firstnameInput'], 
-                    lastname = post['lastnameInput'],
-                    biography = post['biographyInput'],
-                    profile_picture = post['profilePictureInput'],
-                )
-                new_user.save()
-                messages.success(request, "Account created successfully.")
-                return redirect('/authentication/signin/')   
-        return render(request, 'signup.html')
+
+        if CustomUser.objects.filter(email=post['emailInput']).exists():
+            messages.error(request, "Account with this email address already exists.")
+            post_data = {key: value for key, value in post.items() if key != 'emailInput'}
+            post_data['errorType'] = 'emailExists'
+            return render(request, 'signup.html', {'post': post_data})
+
+        if len(post['emailInput']) >= 30:
+            messages.error(request, "Email address is too long.")
+            post_data = {key: value for key, value in post.items() if key != 'emailInput'}
+            post_data['errorType'] = 'emailAddressTooLong'
+            return render(request, 'signup.html', {'post': post_data})
+        
+        if post['passwordInput'] != post['passwordInput2']:
+            messages.error(request, "You repeated the wrong password. Please try again.")
+            post_data = {key: value for key, value in post.items() if key != 'passwordInput2'}
+            post_data['errorType'] = 'passwordUnequal'
+            return render(request, 'signup.html', {'post': post_data})
+
+        if len(post['passwordInput']) < 10 or not any(char.isalpha() for char in post['passwordInput']) or not any(char.isdigit() for char in post['passwordInput']):
+            messages.error(request, "Password is too short or/and includes no numbers and letters.")
+            post_data = {key: value for key, value in post.items() if key not in ['passwordInput', 'passwordInput2']}
+            post_data['errorType'] = 'invalidPassword'
+            return render(request, 'signup.html', {'post': post_data})
+        
+        if len(post['passwordInput']) >= 40 :
+            messages.error(request, "Sorry but your password is too long.")
+            post_data = {key: value for key, value in post.items() if key not in ['passwordInput', 'passwordInput2']}
+            post_data['errorType'] = 'passwordTooLong'
+            return render(request, 'signup.html', {'post': post_data})
+        
+        if len(post['firstnameInput']) > 20 :
+            messages.error(request, "Your firstname is too long. Try to shorten it.")
+            post_data = {key: value for key, value in post.items() if key != 'firstnameInput'}
+            post_data['errorType'] = 'firstnameTooLong'
+            return render(request, 'signup.html', {'post': post_data})
+        
+        if len(post['lastnameInput']) > 20 :
+            messages.error(request, "Your lastname is too long. Try to shorten it.")
+            post_data = {key: value for key, value in post.items() if key != 'lastnameInput'}
+            post_data['errorType'] = 'lastnameTooLong'
+            return render(request, 'signup.html', {'post': post_data})
+        
+        if len(post['biographyInput']) > 150 :
+            messages.error(request, "Your biography is too long. Maximum 150 characters!")
+            post_data = {key: value for key, value in post.items() if key != 'biographyInput'}
+            post_data['errorType'] = 'biographyTooLong'
+            return render(request, 'signup.html', {'post': post_data})
+
+        new_user = CustomUser.objects.create_user(
+            email=post['emailInput'],
+            password=post['passwordInput'],
+            firstname=post['firstnameInput'],
+            lastname=post['lastnameInput'],
+            biography=post['biographyInput'],
+            profile_picture=post['profilePictureInput']
+        )
+        new_user.save()
+        messages.success(request, "Account created successfully.")
+        return redirect('signup_pp')
+
+    return render(request, 'signup.html')
+
 
 def ChooseGroupAction(request):
     if request.user.is_authenticated:
