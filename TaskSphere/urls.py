e02065927ae80315b9fb57d2cf232a977a99e00c
Rehajel <143653636+Rehajel15@users.noreply.github.com>
2024-10-12 23:55:57 +0200

    Replaced the urls of the paths with their names in the redirect functions

--- TaskSphere/urls.py
+++ TaskSphere/urls.py
@@ -5,5 +5,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls, name='admin'),
-    path('authentication/', include('authentication.urls'), name='authentication')
+    path('authentication/', include('authentication.urls'), name='authentication'),
+    path('home/', include('home.urls'), name='home'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- authentication/views.py
+++ authentication/views.py
@@ -13,42 +13,34 @@ def main(request):
 def SignIn(request):
     if request.user.is_authenticated:
         messages.error(request, f"You can't sign in because you already are. To do this please sign out.")
-        return redirect('/authentication/')
+        return redirect("main")
     else:
         if request.method == 'POST':
             email = request.POST['emailInput']
             password = request.POST['passwordInput']
-            errorType = None
 
             if len(email) > 30:
                 messages.error(request, 'The entered email address is too long.')
-                errorType = 'emailTooLong'
-                return render(request, 'signin.html', {'errorType': errorType, 'password': password})
-            
-            if len(password) > 100:
-                messages.error(request, 'The entered password is too long.')
-                errorType = 'passwordTooLong'
-                return render(request, 'signin.html', {'errorType': errorType, 'email': email})
-            
+                return render(request, 'signin.html',)
 
             user = authenticate(request, email=email, password=password)
 
             if user is None:
                 messages.error(request, "Wrong email or password. Please try again.")
-                return redirect('/authentication/signin/')
+                return redirect('signin')
             else:
                 login(request, user)
-                if user.in_group is None:  # in_group is empty
+                if user.group is None:  # in_group is empty
                     return redirect('/authentication/signup/choosegroupaction')
                 else:
-                    return redirect('/home')
+                    return redirect('home')
                 
     return render(request, 'signin.html')
 
 def SignUp(request):
     if request.user.is_authenticated:
         messages.error(request, "You can't sign up because you already are. To do this please sign out.")
-        return redirect('/authentication/')
+        return redirect('main')
     
     signUp_form = SignUpForm()
 
@@ -57,7 +49,7 @@ def SignUp(request):
         if signUp_form.is_valid():
             signUp_form.save()
             messages.success(request, "Account created successfully.")
-            return redirect('/authentication')
+            return redirect('main')
     
     return render(request, 'signup.html', {'signUp_form': signUp_form,})
 
@@ -66,25 +58,25 @@ def ChooseGroupAction(request):
     if request.user.is_authenticated:
         if request.user.in_group is not None:
             messages.error(request, "You already part of a group. Please leave the group first.")
-            return redirect('/authentication/')
+            return redirect('main')
         else:
             return render(request, 'chooseGroupAction.html')
     else:
         messages.error(request, "To have access to this page you need to sign in.")
-        return redirect('/authentication/signin/')
+        return redirect('signin')
 
 def JoinGroup(request):
     if request.user.is_authenticated:
         if request.user.in_group is not None:
             messages.error(request, "You already part of a group. Please leave the group first.")
-            return redirect('/authentication/')
+            return redirect('main')
         else:
             return render(request, 'joingroup.html')
     else:
         messages.error(request, "To have access to this page you need to sign in.")
-        return redirect('/authentication/signin/')
+        return redirect('signin')
 
 def SignOut(request):
     logout(request)
     messages.success(request, 'Successfully logged out.')
-    return redirect('/authentication/signin/')
+    return redirect('signin')
