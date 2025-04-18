cb77cc33e756e11598dc927204dd538e3ecc8bc9
Rehajel <143653636+Rehajel15@users.noreply.github.com>
2025-04-18 21:05:54 +0200

    Fixed logging out bug

--- authentication/views.py
+++ authentication/views.py
@@ -136,7 +136,7 @@ def DeleteAccount(request):
                 if request.user.group is not None:
                     request.user.group == None
                 logout(request)
-                User.objects.get(email = request.user.email).delete()
+                User.objects.get(email = user.email).delete()
                 # -----------------Send user email to confirm the deletion of the account--------------------------
                 messages.success(request, "Deleted your account successfully. We hope to see you again soon!")
                 return redirect("main")
