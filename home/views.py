4e73913f57312f145bf706f7de64d002736c395f
Rehajel <143653636+Rehajel15@users.noreply.github.com>
2025-08-06 01:15:03 +0200

    Worked on the table design + created the first version of a task.

--- home/views.py
+++ home/views.py
@@ -1,5 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 
 # Create your views here.
 def main(request):
-    return render(request, 'home/main.html')
+    if not request.user.is_authenticated:
+        messages.error(request, "To have access to this page you need to sign in.")
+        return redirect('authentication:signin')
+    elif request.user.group is None:
+        messages.error(request, "To have access to this page you need to be part of a group.")
+        return redirect('authentication:choosegroupaction')
+    else:
+        # Assuming the user has a group and the group has a table
+        group_table = request.user.group.table 
+        return render(request, 'home/main.html', {"table":group_table})
